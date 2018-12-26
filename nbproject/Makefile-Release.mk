984af7b8642d205167078c9a10088c1ed0f276fa
Kevin Becker <kbecke35@ford.com>
2018-12-25 20:16:24 -0500

    Tweaks while trying to get NB UT working on alternate machine. No change at all to code.

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -39,10 +39,21 @@ OBJECTFILES= \
 	${OBJECTDIR}/cwsw_arch/i386/peripheral/ports/ports_api.o \
 	${OBJECTDIR}/cwsw_arch/i386/src/cwsw_arch.o \
 	${OBJECTDIR}/cwsw_board/none/src/cwsw_board.o \
-	${OBJECTDIR}/cwsw_lib/src/cwsw_lib.o \
-	${OBJECTDIR}/ut/ut_arch/cwsw_arch_test.o \
-	${OBJECTDIR}/ut/ut_lib/cwsw_lib_test.o
+	${OBJECTDIR}/cwsw_lib/src/cwsw_lib.o
 
+# Test Directory
+TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
+
+# Test Files
+TESTFILES= \
+	${TESTDIR}/TestFiles/f2 \
+	${TESTDIR}/TestFiles/f1 \
+	${TESTDIR}/TestFiles/f3
+
+# Test Object Files
+TESTOBJECTFILES= \
+	${TESTDIR}/ut/ut_arch/cwsw_arch_test.o \
+	${TESTDIR}/ut/ut_lib/cwsw_lib_test.o
 
 # C Compiler Flags
 CFLAGS=
@@ -93,18 +104,113 @@ ${OBJECTDIR}/cwsw_lib/src/cwsw_lib.o: cwsw_lib/src/cwsw_lib.c
 	${RM} "$@.d"
 	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cwsw_lib/src/cwsw_lib.o cwsw_lib/src/cwsw_lib.c
 
-${OBJECTDIR}/ut/ut_arch/cwsw_arch_test.o: ut/ut_arch/cwsw_arch_test.c
-	${MKDIR} -p ${OBJECTDIR}/ut/ut_arch
-	${RM} "$@.d"
-	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ut/ut_arch/cwsw_arch_test.o ut/ut_arch/cwsw_arch_test.c
-
-${OBJECTDIR}/ut/ut_lib/cwsw_lib_test.o: ut/ut_lib/cwsw_lib_test.c
-	${MKDIR} -p ${OBJECTDIR}/ut/ut_lib
-	${RM} "$@.d"
-	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ut/ut_lib/cwsw_lib_test.o ut/ut_lib/cwsw_lib_test.c
-
 # Subprojects
 .build-subprojects:
+
+# Build Test Targets
+.build-tests-conf: .build-tests-subprojects .build-conf ${TESTFILES}
+.build-tests-subprojects:
+
+${TESTDIR}/TestFiles/f2: ${TESTDIR}/ut/ut_arch/cwsw_arch_test.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.c} -o ${TESTDIR}/TestFiles/f2 $^ ${LDLIBSOPTIONS}   
+
+${TESTDIR}/TestFiles/f1: ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.c} -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS}   
+
+${TESTDIR}/TestFiles/f3: ${TESTDIR}/ut/ut_lib/cwsw_lib_test.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.c} -o ${TESTDIR}/TestFiles/f3 $^ ${LDLIBSOPTIONS}   
+
+
+${TESTDIR}/ut/ut_arch/cwsw_arch_test.o: ut/ut_arch/cwsw_arch_test.c 
+	${MKDIR} -p ${TESTDIR}/ut/ut_arch
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/ut/ut_arch/cwsw_arch_test.o ut/ut_arch/cwsw_arch_test.c
+
+
+${TESTDIR}/ut/ut_lib/cwsw_lib_test.o: ut/ut_lib/cwsw_lib_test.c 
+	${MKDIR} -p ${TESTDIR}/ut/ut_lib
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/ut/ut_lib/cwsw_lib_test.o ut/ut_lib/cwsw_lib_test.c
+
+
+${OBJECTDIR}/app/main_nomain.o: ${OBJECTDIR}/app/main.o app/main.c 
+	${MKDIR} -p ${OBJECTDIR}/app
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/app/main.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -O2 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/app/main_nomain.o app/main.c;\
+	else  \
+	    ${CP} ${OBJECTDIR}/app/main.o ${OBJECTDIR}/app/main_nomain.o;\
+	fi
+
+${OBJECTDIR}/cwsw_arch/i386/peripheral/ports/ports_api_nomain.o: ${OBJECTDIR}/cwsw_arch/i386/peripheral/ports/ports_api.o cwsw_arch/i386/peripheral/ports/ports_api.c 
+	${MKDIR} -p ${OBJECTDIR}/cwsw_arch/i386/peripheral/ports
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/cwsw_arch/i386/peripheral/ports/ports_api.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -O2 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cwsw_arch/i386/peripheral/ports/ports_api_nomain.o cwsw_arch/i386/peripheral/ports/ports_api.c;\
+	else  \
+	    ${CP} ${OBJECTDIR}/cwsw_arch/i386/peripheral/ports/ports_api.o ${OBJECTDIR}/cwsw_arch/i386/peripheral/ports/ports_api_nomain.o;\
+	fi
+
+${OBJECTDIR}/cwsw_arch/i386/src/cwsw_arch_nomain.o: ${OBJECTDIR}/cwsw_arch/i386/src/cwsw_arch.o cwsw_arch/i386/src/cwsw_arch.c 
+	${MKDIR} -p ${OBJECTDIR}/cwsw_arch/i386/src
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/cwsw_arch/i386/src/cwsw_arch.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -O2 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cwsw_arch/i386/src/cwsw_arch_nomain.o cwsw_arch/i386/src/cwsw_arch.c;\
+	else  \
+	    ${CP} ${OBJECTDIR}/cwsw_arch/i386/src/cwsw_arch.o ${OBJECTDIR}/cwsw_arch/i386/src/cwsw_arch_nomain.o;\
+	fi
+
+${OBJECTDIR}/cwsw_board/none/src/cwsw_board_nomain.o: ${OBJECTDIR}/cwsw_board/none/src/cwsw_board.o cwsw_board/none/src/cwsw_board.c 
+	${MKDIR} -p ${OBJECTDIR}/cwsw_board/none/src
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/cwsw_board/none/src/cwsw_board.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -O2 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cwsw_board/none/src/cwsw_board_nomain.o cwsw_board/none/src/cwsw_board.c;\
+	else  \
+	    ${CP} ${OBJECTDIR}/cwsw_board/none/src/cwsw_board.o ${OBJECTDIR}/cwsw_board/none/src/cwsw_board_nomain.o;\
+	fi
+
+${OBJECTDIR}/cwsw_lib/src/cwsw_lib_nomain.o: ${OBJECTDIR}/cwsw_lib/src/cwsw_lib.o cwsw_lib/src/cwsw_lib.c 
+	${MKDIR} -p ${OBJECTDIR}/cwsw_lib/src
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/cwsw_lib/src/cwsw_lib.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.c) -O2 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/cwsw_lib/src/cwsw_lib_nomain.o cwsw_lib/src/cwsw_lib.c;\
+	else  \
+	    ${CP} ${OBJECTDIR}/cwsw_lib/src/cwsw_lib.o ${OBJECTDIR}/cwsw_lib/src/cwsw_lib_nomain.o;\
+	fi
+
+# Run Test Targets
+.test-conf:
+	@if [ "${TEST}" = "" ]; \
+	then  \
+	    ${TESTDIR}/TestFiles/f2 || true; \
+	    ${TESTDIR}/TestFiles/f1 || true; \
+	    ${TESTDIR}/TestFiles/f3 || true; \
+	else  \
+	    ./${TEST} || true; \
+	fi
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
